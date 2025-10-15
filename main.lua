5a6e6c042b246bdf156a132df4b7d3b324f83402
punxch <punxch@gmail.com>
2025-10-15 23:51:47 +0800

    use vimg

--- main.lua
+++ main.lua
@@ -101,34 +101,34 @@ function M:preload(job)
 	-- }):status()
 
 	-- method 3
-	-- local cmd = Command("vimg"):arg({
-	-- 	"vcs", "-c3", "-H288", "-n9", "-f1"
-	-- })
-
-	-- local status, err = cmd:arg({
-	-- 	tostring(job.file.url), "--output",
-	-- 	tostring(cache)
-	-- }):status()
-
-	-- method 4
-	local cmd = Command("ffmpeg"):arg({
-		"-v", "quiet", "-threads", 1, "-hwaccel", "auto",
-		"-skip_frame", "nokey",
-		"-an", "-sn", "-dn",
+	local cmd = Command("vimg"):arg({
+		"vcs", "-c3", "-H288", "-n9", "-f1"
 	})
 
-	cmd:arg { "-i", tostring(job.file.url) }
-
-	-- -- stylua: ignore
 	local status, err = cmd:arg({
-		"-frames:v", 1,
-		"-q:v", 31 - math.floor(rt.preview.image_quality * 0.3),
-		"-vf", 
-		string.format("select=isnan(prev_selected_t)+gte(t-prev_selected_t\\,%d),scale=-1:288:force_original_aspect_ratio=decrease:flags=fast_bilinear,drawtext=text=\'%%{pts\\:gmtime\\:0\\:%%H\\\\\\:%%M\\\\\\:%%S}\':fontcolor=white:fontsize=40:box=1:boxcolor=black@0.5:boxborderw=1:x=w-text_w-2:y=h-text_h-2,tile=3x3", math.floor(meta.format.duration/9)),
-		"-vsync", 0,
-		"-f", "image2",
-		"-y", tostring(cache),
+		tostring(job.file.url), "--output",
+		tostring(cache)
 	}):status()
+
+	-- -- method 4
+	-- local cmd = Command("ffmpeg"):arg({
+	-- 	"-v", "quiet", "-threads", 1, "-hwaccel", "auto",
+	-- 	"-skip_frame", "nokey",
+	-- 	"-an", "-sn", "-dn",
+	-- })
+
+	-- cmd:arg { "-i", tostring(job.file.url) }
+
+	-- -- -- stylua: ignore
+	-- local status, err = cmd:arg({
+	-- 	"-frames:v", 1,
+	-- 	"-q:v", 31 - math.floor(rt.preview.image_quality * 0.3),
+	-- 	"-vf", 
+	-- 	string.format("select=isnan(prev_selected_t)+gte(t-prev_selected_t\\,%d),scale=-1:288:force_original_aspect_ratio=decrease:flags=fast_bilinear,drawtext=text=\'%%{pts\\:gmtime\\:0\\:%%H\\\\\\:%%M\\\\\\:%%S}\':fontcolor=white:fontsize=40:box=1:boxcolor=black@0.5:boxborderw=1:x=w-text_w-2:y=h-text_h-2,tile=3x3", math.floor(meta.format.duration/9)),
+	-- 	"-vsync", 0,
+	-- 	"-f", "image2",
+	-- 	"-y", tostring(cache),
+	-- }):status()
 
 	if not status then
 		return true, Err("Failed to start `ffmpeg`, error: %s", err)
